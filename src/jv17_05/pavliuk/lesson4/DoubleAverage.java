1f832e331023902afdd0f465ff73216879160f99
ospavliuk <asp4rever@gmail.com>
2017-09-11 16:36:51 +0300

    third task completed;

---
+++ src/jv17_05/pavliuk/lesson4/DoubleAverage.java
@@ -0,0 +1,13 @@
+package jv17_05.pavliuk.lesson4;
+
+public class DoubleAverage {
+    public static void main(String[] args) {
+        double sum = 0;
+        for (String c : args) {
+            sum += Double.parseDouble(c);
+        }
+        double result = 0;
+        result = args.length != 0 ? sum / args.length : 0;
+        System.out.println("Среднее арифметическое значение от переданных аргументов = " + result);
+    }
+}
