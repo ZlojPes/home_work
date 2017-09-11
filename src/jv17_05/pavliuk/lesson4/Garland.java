3dc1263ff7ed7594ed9e23988955f6b68008b3cd
ospavliuk <asp4rever@gmail.com>
2017-09-11 15:03:18 +0300

    second task fixed;

---
+++ src/jv17_05/pavliuk/lesson4/Garland.java
@@ -0,0 +1,23 @@
+package jv17_05.pavliuk.lesson4;
+
+public class Garland {
+    private static int garland = -2004318072;
+
+    public static void main(String[] args) {
+        System.out.println(Integer.toBinaryString(garland));
+        blink(9);
+        System.out.println(Integer.toBinaryString(garland));
+        moveLeft(11);
+        System.out.println(Integer.toBinaryString(garland));
+    }
+
+    static void blink(int count) {
+        for (int i = 0; i < count; i++) {
+            garland = ~garland;
+        }
+    }
+
+    static void moveLeft(int numberOfMoves) {
+        garland <<= numberOfMoves;
+    }
+}
