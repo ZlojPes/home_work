d7b6c9a6b925df82474b19604bad2fec21514d54
Victor Kucherenko <Ku4erenko.V.V@yandex.ru>
2017-09-17 11:27:26 +0300

    ClassWork

---
+++ src/jv17_05/pavliuk/lesson6/Array3x6.java
@@ -0,0 +1,24 @@
+package jv17_05.pavliuk.lesson6;
+
+/**
+ * Created by student on 17-Sep-17.
+ */
+public class Array3x6 {
+    public static void main(String[] args) {
+        String[][] ar = new String[3][6];
+        char ch = 'a';
+        for (int i = 0; i < ar.length; i++) {
+            for (int j = 0; j < ar[i].length; j++) {
+                ar[i][j] = "" + ch + (j + 1);
+            }
+            ch++;
+        }
+
+        for (String[] s : ar) {
+            for (String p : s) {
+                System.out.print(p + " ");
+            }
+            System.out.println();
+        }
+    }
+}
