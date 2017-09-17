85785afd8bb311252310c0c029b9841edbc98e5a
Victor Kucherenko <Ku4erenko.V.V@yandex.ru>
2017-09-17 10:52:06 +0300

    ClassWork

---
+++ src/jv17_05/pavliuk/lesson6/WeekArray.java
@@ -0,0 +1,18 @@
+package jv17_05.pavliuk.lesson6;
+
+/**
+ * Created by student on 17-Sep-17.
+ */
+public class WeekArray {
+    public static void main(String[] args) {
+        String[] week = new String[7];
+        week[0] = "Sunday";
+        week[1] = "Monday";
+        week[2] = "Tuesday";
+        week[3] = "Wednesday";
+        week[4] = "Thuersday";
+        week[5] = "Friday";
+        week[6] = "Saturday";
+        System.out.print(week[6]);
+    }
+}
