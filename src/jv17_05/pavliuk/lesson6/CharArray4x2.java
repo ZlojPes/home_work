3cf151ee71cc99855c61620eba9edd8158e747b3
ospavliuk <asp4rever@gmail.com>
2017-09-18 17:16:34 +0300

    lesson6 updated.

---
+++ src/jv17_05/pavliuk/lesson6/CharArray4x2.java
@@ -0,0 +1,10 @@
+package jv17_05.pavliuk.lesson6;
+
+import java.util.Arrays;
+
+public class CharArray4x2 {
+    public static void main(String[] args) {
+        char[][] array = {{'a', 'b'}, {'c', 'd'}, {'e', 'f'}, {'g', 'h'},};
+        System.out.println(Arrays.deepToString(array));
+    }
+}
