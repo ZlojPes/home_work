5dcbd9c24f825b2c8baa28037fa802abad19074f
ZlojPes <asp4rever@gmail.com>
2017-09-19 23:07:02 +0300

    sort() is reversed and forced.

--- src/jv17_05/pavliuk/lesson7/BubbleSorter.java
+++ src/jv17_05/pavliuk/lesson7/BubbleSorter.java
@@ -4,20 +4,25 @@ import java.util.Arrays;
 
 public class BubbleSorter {
     public static void main(String[] args) {
-        int[] array = {4, 2, 8, 45, 3, 23, 1};
-        System.out.println("Орининальный массив:" + Arrays.toString(array));
+        int[] array = {46, 33, 23, 1, 45, 18, 22, 3, 15};
+        System.out.println("Оригинальный массив:" + Arrays.toString(array));
         sort(array);
         System.out.println("Отсортированный массив:" + Arrays.toString(array));
     }
 
     private static void sort(int[] array) {
-        for (int i = array.length - 1; i >0; i--) {
+        for (int i = array.length - 1; i > 0; i--) {
+            boolean hasChanges = false;
             for (int j = 0; j < i; j++) {
                 if (array[j + 1] > array[j]) {
                     int tmp = array[j + 1];
                     array[j + 1] = array[j];
                     array[j] = tmp;
+                    hasChanges = true;
                 }
+            }
+            if (!hasChanges) {
+                return;
             }
         }
     }
