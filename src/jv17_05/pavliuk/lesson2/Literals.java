fc0e53b8aa6289edefe2c7460578a4262eeb28ba
ospavliuk <asp4rever@gmail.com>
2017-09-19 13:21:00 +0300

    Some 2,3 lesson's tasks have been fixed.

--- src/jv17_05/pavliuk/lesson2/Literals.java
+++ src/jv17_05/pavliuk/lesson2/Literals.java
@@ -1,14 +1,13 @@
 package jv17_05.pavliuk.lesson2;
 
+/**
+ * Добавление в объектный массив различных примитивных типов
+ * с пследующим выводом их в консоли.
+ */
+
 public class Literals {
-
-    /**
-     * Добавление в объектный массив различных примитивных типов
-     * с пследующим выводом их в консоли.
-     */
-
     public static void main(String[] args) {
-        Object[] literals = new Object[]{true, "String", 'c', 0b10, 010, 10, 0x10, 3.1415926f, 2.71828182}; //    Литералы в Java
+        Object[] literals = new Object[]{true, "String", 'c', 0b10, 010, 10, 0x10, 3.1415926f, 2.71828182};
         for (Object o : literals) {
             System.out.print(o + "; ");
         }
--- src/jv17_05/pavliuk/lesson3/PrintSum.java
+++ src/jv17_05/pavliuk/lesson3/PrintSum.java
@@ -5,21 +5,20 @@ import java.util.Scanner;
 public class PrintSum {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int i1 = 0, i2 = 0;
-        System.out.print("Введите первое слагаемое (целое число)");
-        if (scanner.hasNextInt()) {
-            i1 = scanner.nextInt();
-        } else {
-            System.out.print("Вы ввели не целое число!");
-            System.exit(1);
-        }
-        System.out.print("Введите второе слагаемое (целое число)");
-        if (scanner.hasNextInt()) {
-            i2 = scanner.nextInt();
-        } else {
-            System.out.print("Вы ввели не целое число!");
-            System.exit(1);
-        }
+        int i1 = getUserInput(scanner);
+        int i2 = getUserInput(scanner);
         System.out.print("" + i1 + "+" + i2 + "=" + (i1 + i2));
+    }
+
+    private static int getUserInput(Scanner scanner) {
+        while (true) {
+            System.out.print("Введите слагаемое (целое число)");
+            if (scanner.hasNextInt()) {
+                return scanner.nextInt();
+            } else {
+                System.out.print("Вы ввели не целое число!");
+                scanner.nextLine();
+            }
+        }
     }
 }
