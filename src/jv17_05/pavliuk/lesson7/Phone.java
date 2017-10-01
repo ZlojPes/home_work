e4972218c1ef9285a2533046f93db285d1822468
t-vieru <t_vieru@hotmail.com>
2017-10-01 11:27:52 +0300

    Phone upgrated

--- src/jv17_05/pavliuk/lesson7/Phone.java
+++ src/jv17_05/pavliuk/lesson7/Phone.java
@@ -5,10 +5,22 @@ public class Phone {
     private int weight;
     private String model;
 
+    public Phone() {
+    }
+
     public Phone(long number, int weight, String model) {
+        this();
         this.number = number;
         this.weight = weight;
         this.model = model;
+    }
+
+    private void receiveCall(String callerName) {
+        System.out.println("Звонит " + callerName);
+    }
+
+    private void receiveCall(String callerName, long number) {
+        System.out.println("Звонит " + callerName + " {" + number + "}");
     }
 
     public long getNumber() {
@@ -31,9 +43,11 @@ public class Phone {
     public static void main(String[] args) {
         Phone phone1 = new Phone(380971234567L, 93, "Nokia 1100");
         Phone phone2 = new Phone(380577231598L, 1087, "VEF-72 (дисковый)");
-        Phone phone3 = new Phone(380990516273L, 174, "IPhone X");
+        Phone phone3 = new Phone(380990516273L, 174, "Apple IPhone X");
         System.out.println(phone1 + "\n");
         System.out.println(phone2 + "\n");
         System.out.println(phone3);
+
+        phone1.receiveCall("Петя", 380571234567L);
     }
 }
