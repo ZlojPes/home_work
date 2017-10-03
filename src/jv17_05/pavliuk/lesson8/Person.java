bb8947373179bef60d4187e2e51a1f23cd46a55b
ospavliuk <asp4rever@gmail.com>
2017-10-03 15:12:49 +0300

    lesson8 finished.

--- src/jv17_05/pavliuk/lesson8/Person.java
+++ src/jv17_05/pavliuk/lesson8/Person.java
@@ -5,7 +5,7 @@ public class Person {
     int age;
 
     public Person() {
-        fullName = "Not specified";
+        fullName = "NoName";
         age = 0;
     }
 
@@ -14,8 +14,19 @@ public class Person {
         this.age = age;
     }
 
+    public void move() {
+        System.out.println(fullName + " движется к своей цели");
+    }
+
+    public void talk() {
+        System.out.println(fullName + " говорит о Java");
+    }
+
     public static void main(String[] args) {
         Person first = new Person();
         Person second = new Person("Александр Павлюк", 35);
+        first.talk();
+        second.move();
+        second.talk();
     }
 }
