82f6920bfef39ca449bc7dc96fc640faef016309
ospavliuk <asp4rever@gmail.com>
2017-10-09 14:29:02 +0300

    Updated.

--- src/jv17_05/pavliuk/lesson10/Recursion.java
+++ src/jv17_05/pavliuk/lesson10/Recursion.java
@@ -1,14 +1,14 @@
 package jv17_05.pavliuk.lesson10;
 
 public class Recursion {
-    int start, end;
+    private int start, end;
 
     public Recursion(int start, int end) {
         this.start = start;
         this.end = end;
     }
 
-    void print(int current) {
+    public void print(int current) {
         System.out.println(current);
         if (current++ < end) {
             print(current);
@@ -17,10 +17,6 @@ public class Recursion {
 
     public int getStart() {
         return start;
-    }
-
-    public int getEnd() {
-        return end;
     }
 
     public static void main(String[] args) {
