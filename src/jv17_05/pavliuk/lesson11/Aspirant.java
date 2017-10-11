9ade3d93e7352487ddda7bc7c6de68625d767cfd
dhodyrev <dhodyrev@gmail.com>
2017-10-11 21:30:53 +0300

    klasswork

---
+++ src/jv17_05/pavliuk/lesson11/Aspirant.java
@@ -0,0 +1,17 @@
+package jv17_05.pavliuk.lesson11;
+
+/**
+ * Created by Student on 11.10.2017.
+ */
+public class Aspirant extends Student {
+    private String scienseWork;
+
+    public Aspirant(String firstName, String lastName, String group, String scienseWork) {
+        super(firstName, lastName, group);
+        this.scienseWork = scienseWork;
+    }
+
+    public int getScholarship(){
+        return 200;
+    }
+}
---
+++ src/jv17_05/pavliuk/lesson11/Student.java
@@ -0,0 +1,22 @@
+package jv17_05.pavliuk.lesson11;
+
+/**
+ * Created by Student on 11.10.2017.
+ */
+public class Student {
+    public String firstName, lastName, group;
+
+    public Student(String firstName, String lastName, String group) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.group = group;
+    }
+
+    public int getScholarship(){
+        return 100;
+    }
+
+    public String toString(){
+        return firstName+" "+lastName+" Группа: "+group;
+    }
+}
