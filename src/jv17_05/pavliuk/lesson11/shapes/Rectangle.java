89b7f34becf1cfeab2e8a4f3b050433d9c04b733
ZlojPes <asp4rever@gmail.com>
2017-10-16 00:28:25 +0300

    shapes is finished;

--- src/jv17_05/pavliuk/lesson11/shapes/Rectangle.java
+++ src/jv17_05/pavliuk/lesson11/shapes/Rectangle.java
@@ -4,8 +4,13 @@ import java.util.Arrays;
 
 public class Rectangle extends Shape {
     private int[] a, b, c, d;
-    public Rectangle(String color){
+
+    public Rectangle(String color, int[] a, int[] b, int[] c, int[] d) {
         super(color);
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
     }
 
     public int[] getA() {
@@ -42,8 +47,17 @@ public class Rectangle extends Shape {
 
     @Override
     public void draw() {
-        System.out.println("фигура квадрат с координатами а:"+ Arrays.toString(a)+" b:"+ Arrays.toString(b)+" c:"+
-                Arrays.toString(c)+" d:"+ Arrays.toString(a));
+        System.out.println("Draw " + this);
+    }
+
+    @Override
+    public String toString() {
+        return color + " rectangle{" +
+                "a=" + Arrays.toString(a) +
+                ", b=" + Arrays.toString(b) +
+                ", c=" + Arrays.toString(c) +
+                ", d=" + Arrays.toString(d) +
+                '}';
     }
 
     @Override
@@ -53,11 +67,11 @@ public class Rectangle extends Shape {
 
         Rectangle rectangle = (Rectangle) o;
 
-        if (!Arrays.equals(a, rectangle.getA())) return false;
-        if (!Arrays.equals(b, rectangle.getB())) return false;
-        if (!Arrays.equals(c, rectangle.getC())) return false;
-        return Arrays.equals(d, rectangle.getD());
-
+        if (!color.equals(rectangle.color)) return false;
+        if (!Arrays.equals(a, rectangle.a)) return false;
+        if (!Arrays.equals(b, rectangle.b)) return false;
+        if (!Arrays.equals(c, rectangle.c)) return false;
+        return Arrays.equals(d, rectangle.d);
     }
 
     @Override
