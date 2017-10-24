14ac2360d122a8b46796278010bba104a31fbeeb
ospavliuk <asp4rever@gmail.com>
2017-10-24 12:05:40 +0300

    clothes finished;

--- src/jv17_05/pavliuk/lesson13/clothes/Atelier.java
+++ src/jv17_05/pavliuk/lesson13/clothes/Atelier.java
@@ -1,7 +1,34 @@
 package jv17_05.pavliuk.lesson13.clothes;
 
 public class Atelier {
-    public void dressMan(Clothes[] collection){
+    public void dressMan(Clothes[] collection) {
+        for (Clothes clothes : collection) {
+            try {
+                ((ManClothes) clothes).dressMan();
+                System.out.println(clothes);
+            } catch (ClassCastException e) {
+            }
+        }
+    }
 
+    public void dressWoman(Clothes[] collection) {
+        for (Clothes clothes : collection) {
+            try {
+                ((WomanClothes) clothes).dressWoman();
+                System.out.println(clothes);
+            } catch (ClassCastException e) {
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Atelier atelier = new Atelier();
+        TShirt tShirt = new TShirt(Size.S, 150, "White");
+        Pants pants = new Pants(Size.L, 200, "Blue");
+        Skirt skirt = new Skirt(Size.XS, 250, "Yellow");
+        Tie tie = new Tie(Size.XXS, 300, "Black");
+        Clothes[] collection = {tShirt, pants, skirt, tie};
+        atelier.dressMan(collection);
+        atelier.dressWoman(collection);
     }
 }
