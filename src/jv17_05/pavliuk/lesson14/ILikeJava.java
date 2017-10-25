b2d0a65e87f6b806a020bed85b197df5f99bae78
dhodyrev <dhodyrev@gmail.com>
2017-10-25 21:22:45 +0300

    lesson14

---
+++ src/jv17_05/pavliuk/lesson14/ILikeJava.java
@@ -0,0 +1,21 @@
+package jv17_05.pavliuk.lesson14;
+
+/**
+ * Created by Student on 25.10.2017.
+ */
+public class ILikeJava {
+    public static void main(String[] args) {
+        test("I like Java!!!");
+    }
+
+    public static void test(String str){
+        System.out.println(str.charAt(str.length()-1));
+        System.out.println(str.endsWith("!!!"));
+        System.out.println(str.startsWith("I li"));
+        System.out.println(str.indexOf("Java"));
+        System.out.println(str.replace('a','o'));
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+        System.out.println(str.substring(7,11));
+    }
+}
