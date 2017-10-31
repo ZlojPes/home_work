8c13445011b67b7a49de2230a927f11972e2ba59
ospavliuk <asp4rever@gmail.com>
2017-10-31 16:03:42 +0200

    Employee has been added

--- src/jv17_05/pavliuk/lesson15/employee/Report.java
+++ src/jv17_05/pavliuk/lesson15/employee/Report.java
@@ -5,13 +5,20 @@ public class Report {
     }
 
     public static void generateReport(Employee[] employees) {
-        int count = 0;
+        int count = 0, nameMaxLength = 0;
         float totalSalary = 0;
         for (Employee emp : employees) {
+            int l = emp.getFullName().length();
+            if (l > nameMaxLength) {
+                nameMaxLength = l;
+            }
+        }
+        System.out.printf("%-" + (nameMaxLength + 6) + "s%s%n", "n/n Employee", "Salary"); //Header
+        for (Employee emp : employees) {
             count++;
-            System.out.printf("%d). %s has salary %.2f$%n", count, emp.getFullName(), emp.getSalary());
+            System.out.printf(" %-3d%-" + nameMaxLength + "s  %.2f$%n", count, emp.getFullName(), emp.getSalary());
             totalSalary += emp.getSalary();
         }
-        System.out.printf("Total salary is: %.2f$",totalSalary);
+        System.out.printf("%" + (nameMaxLength + 6) + "s%.2f$", "Total: ", totalSalary);  //Total string
     }
 }
--- src/jv17_05/pavliuk/lesson15/employee/TestEmployee.java
+++ src/jv17_05/pavliuk/lesson15/employee/TestEmployee.java
@@ -3,9 +3,9 @@ package jv17_05.pavliuk.lesson15.employee;
 public class TestEmployee {
     public static void main(String[] args) {
         Employee e1 = new Employee("Ivanov Petr", getSalary());
-        Employee e2 = new Employee("Petrov Ivan", getSalary());
+        Employee e2 = new Employee("Smirnova Marina", getSalary());
         Employee e3 = new Employee("Sidorov Alexey", getSalary());
-        Employee e4 = new Employee("Smirnova Elena", getSalary());
+        Employee e4 = new Employee("Petrov Ivan", getSalary());
         Employee[] array = {e1, e2, e3, e4};
         Report.generateReport(array);
     }
