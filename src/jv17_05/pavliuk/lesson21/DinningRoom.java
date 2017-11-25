6ce18b422300944f587dee76ea0232ca60e9a41b
Александр (Tablet PC) <asp4rever@gmail.com>
2017-11-25 21:21:40 +0200

    corrected

--- src/jv17_05/pavliuk/lesson21/DinningRoom.java
+++ src/jv17_05/pavliuk/lesson21/DinningRoom.java
@@ -3,10 +3,28 @@ package jv17_05.pavliuk.lesson21;
 import java.util.*;
 
 public class DinningRoom {
-    Date date = new Date();
+    public static long start = new Date().getTime();
 
     public static void main(String[] args) {
         DinningRoom dinningRoom = new DinningRoom();
-        Set<Philosopher> philosophers = new HashSet<>();
+        Philosopher[] philosophers = new Philosopher[5];
+
+        List<Fork> forks = new ArrayList<>();
+        for (int i = 1; i <= 5; i++) {
+            forks.add(new Fork(i));
+        }
+        for (int i = 0; i < 5; i++) {
+            philosophers[i] = new Philosopher(i + 1 + "-й философ", forks.get(0), forks.get(1), start);
+            Collections.rotate(forks, 1);
+            philosophers[i].start();
+        }
+        for (int i = 0; i < 5; i++) {
+            try {
+                philosophers[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
+
 }
--- src/jv17_05/pavliuk/lesson21/Philosopher.java
+++ src/jv17_05/pavliuk/lesson21/Philosopher.java
@@ -1,17 +1,21 @@
 package jv17_05.pavliuk.lesson21;
 
+import java.util.Date;
+
 public class Philosopher extends Thread {
     private String philosopherName;
     private final Fork leftFork, rightFork;
+    private long start;
 
-    public Philosopher(String philosopherName, Fork leftFork, Fork rightFork) {
+    public Philosopher(String philosopherName, Fork leftFork, Fork rightFork, long start) {
         this.philosopherName = philosopherName;
         this.leftFork = leftFork;
         this.rightFork = rightFork;
+        this.start = start;
     }
 
     public void run() {
-        while (true) {
+        while (new Date().getTime() - start < 30000) {
             if (leftFork.getNumber() < rightFork.getNumber()) {
                 synchronized (leftFork) {
                     synchronized (rightFork) {
