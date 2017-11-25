01ea39620350cdd54afd303397e93e7ffb9135d6
Александр (Tablet PC) <asp4rever@gmail.com>
2017-11-26 01:30:59 +0200

    corrected

--- src/jv17_05/pavliuk/lesson21/DinningRoom.java
+++ src/jv17_05/pavliuk/lesson21/DinningRoom.java
@@ -3,18 +3,16 @@ package jv17_05.pavliuk.lesson21;
 import java.util.*;
 
 public class DinningRoom {
-    public static long start = new Date().getTime();
+    private static final long START = new Date().getTime();
 
     public static void main(String[] args) {
-        DinningRoom dinningRoom = new DinningRoom();
         Philosopher[] philosophers = new Philosopher[5];
-
         List<Fork> forks = new ArrayList<>();
         for (int i = 1; i <= 5; i++) {
             forks.add(new Fork(i));
         }
         for (int i = 0; i < 5; i++) {
-            philosophers[i] = new Philosopher(i + 1 + "-й философ", forks.get(0), forks.get(1), start);
+            philosophers[i] = new Philosopher(i + 1 + "-й философ", forks.get(0), forks.get(1), START);
             Collections.rotate(forks, 1);
             philosophers[i].start();
         }
@@ -26,5 +24,4 @@ public class DinningRoom {
             }
         }
     }
-
 }
