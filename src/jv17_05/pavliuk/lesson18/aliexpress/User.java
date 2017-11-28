86b83f14d1b05152720c707ac24a400188b2cc87
ospavliuk <asp4rever@gmail.com>
2017-11-28 17:14:51 +0200

    added

---
+++ src/jv17_05/pavliuk/lesson18/aliexpress/User.java
@@ -0,0 +1,15 @@
+package jv17_05.pavliuk.lesson18.aliexpress;
+
+public class User {
+    String user, password;
+
+    public User(String user, String password) {
+        this.user = user;
+        this.password = password;
+    }
+
+    public User() {
+        this.user = "";
+        this.password = "";
+    }
+}
